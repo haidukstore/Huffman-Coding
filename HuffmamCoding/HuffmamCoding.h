2f7072d329108ee26433968f78ddb2b87e762fb5
haidukstore <hayduk.roman@gmail.com>
2019-03-26 19:16:56 +0200

    Initial Commit

---
+++ HuffmamCoding/HuffmamCoding.h
@@ -0,0 +1,19 @@
+//
+//  HuffmamCoding.h
+//  HuffmamCoding
+//
+//  Created by Roman Haiduk on 3/26/19.
+//  Copyright © 2019 Roman Haiduk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HuffmamCoding.
+FOUNDATION_EXPORT double HuffmamCodingVersionNumber;
+
+//! Project version string for HuffmamCoding.
+FOUNDATION_EXPORT const unsigned char HuffmamCodingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HuffmamCoding/PublicHeader.h>
+
+
